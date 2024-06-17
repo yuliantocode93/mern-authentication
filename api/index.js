f647834913ba814766071220bf0d4d0295963908
yulianto <jooliantoo@gmail.com>
2024-06-17 10:23:44 +0700

    add mongo database

--- api/index.js
+++ api/index.js
@@ -1,4 +1,13 @@
 import express from "express";
+import mongoose from "mongoose";
+
+mongoose
+  .connect("mongodb://localhost:27017/mern-auth", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log(err));
 
 const app = express();
 
